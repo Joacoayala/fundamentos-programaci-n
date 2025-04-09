52cf25be256ae7f21ac1d60cdc3ce158b0d0e1f0
Joacoayala <jo.ayalah@duocuc.cl>
2025-04-09 09:43:52 -0400

    Add files via upload

---
+++ clase python 02.py
@@ -0,0 +1,23 @@
+#multiplicacion
+A1 = int(input("ingresa tu primer numero: "))
+B1 = int(input("igresa tu segundo numero: "))
+R01 = A1 * B1 
+print(f"el resultado de la multiplicacion es: {R01}")
+
+#division 
+J1 = int(input("ingresa tu primer numero: "))
+M1 = int(input("ingresa tu segundo numero: "))
+R02 = J1 / M1
+print(f"el resultado de la divison es: {R02}")
+
+#resta
+F2 = int(input("ingresa tu primer numero: "))
+R1 = int(input("ingresa tu segundo numero: "))
+R03 = F2 - R1
+print(f"el resultado de la resta es: {R03}")
+
+#suma
+C1 = int(input("ingresa tu primer numero: "))
+H2 = int(input("ingresa tu segundo numero: "))
+R04 = C1 + H2
+print(f"el resultado de tu suma es: {R04}")
