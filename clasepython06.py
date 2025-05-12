9540da5149317eb46ff4952f9b4c1d097d96924b
Joacoayala <jo.ayalah@duocuc.cl>
2025-05-12 11:09:29 -0400

    Add files via upload

---
+++ clasepython06.py
@@ -0,0 +1,7 @@
+A = int(input("ingresa tu primer numero: "))
+B = int(input("ingresa tu segundo numero: "))
+def suma(A, B):
+    return A + B
+resultado = A + B
+print(f"el resultado de tu suma es {resultado}")
+    
